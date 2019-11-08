36d8269997a60fa7bf8f59816cb27b29d2668e54
iobami <ayobamialadenoye@yahoo.com>
2019-11-08 09:28:00 +0100

    watson session fixed

--- Controllers/socket.js
+++ Controllers/socket.js
@@ -1,22 +1,24 @@
-const { startSession } = require("./sessionController.js");
+const { startSession, getServiceMessage } = require("./sessionController.js");
 
 const createConnection = (io) => {
 
-    io.on('connection', function(socket){
+    io.on('connection', async function(socket){
         console.log('a user connected ', socket.id);
         socket.on('disconnect', function(){
             console.log('user disconnected');
         });
+        const sessionId = await startSession();
+        console.log(sessionId);
 
         socket.on('chat message', function(msg){
             console.log('message: ' + msg);
             // startSession(msg);
             async function message() {
-               const [ reply ] = await startSession(msg);
-               console.log(reply[0].text);
+               const [ reply ] = await getServiceMessage(msg);
+               console.log(reply);
                // await io.emit('chat message', reply[0].text);
                 // sending to individual socketid (private message)
-               await io.to(`${socket.id}`).emit('chat message', reply[0].text);
+               await io.to(`${socket.id}`).emit('chat message', reply);
             }
             return message();
         });
