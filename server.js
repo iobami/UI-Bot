1a4fc52763bb0e1986c3af694e6627034d2ba3c2
iobami <ayobamialadenoye@yahoo.com>
2019-11-11 01:33:42 +0100

    facebook message edit

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.post('/webhook', (req, res) => {
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+            console.log('-----------------------++');
+            console.log(webhook_event.message.text);
         });
 
         // Returns a '200 OK' response to all requests
